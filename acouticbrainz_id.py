f7d1799f2b672caca5bf62124c15e30580458089
David Bainbridge <davidb@waikato.ac.nz>
2022-01-14 10:34:14 +0000

    client-id now read in from .txt file; code extended to generate the metadata.json file as output

--- acouticbrainz_id.py
+++ acouticbrainz_id.py
@@ -11,6 +11,22 @@ import time
 
 # import unicodedata
 
+aid_clientid_filename="acousticid_clientid.txt"
+if not os.path.isfile(aid_clientid_filename):
+    print(f"Failed to find: {aid_clientid_filename}")
+    print(f"Have you copied over {aid_clientid_filename}.in and then edited the 'CHANGEME' content?")
+    sys.exit(1)
+    
+fin = open(aid_clientid_filename,"r")
+aid_clientid = fin.read().strip()
+fin.close()
+
+if aid_clientid == "CHANGEME":
+    print("AcousticID client-id was 'CHANGEME'")
+    print(f"Have you registered an app via https://acousticid.biz and then updated {aid_clientid_filename} to contain that value?")
+    print("Continuing anyway, however it is likely the AcousticID API calls will fail")
+
+
 parser = argparse.ArgumentParser(description='Eurovision Data Compute Audio Features')
 parser.add_argument('--start', type=int, default=1956,
                     help='Start year range of the Eurovision Song Contest')
@@ -46,16 +62,24 @@ for year in opt_filtered_sorted_years:
     
     mp3_files = glob.glob(u'audio/'+str(year)+'/*.mp3')
 
-    prev_time_msecs = time.time_ns() // 1_000_000
+    prev_time_msecs = None
     
-    for mp3_file in mp3_files:
-        src_json_file = mp3_file.replace(".mp3",".json")
-        dst_json_file = src_json_file.replace(".json","-musicbrainz.json")
+    for mp3_filename in mp3_files:
+        src_json_filename = mp3_filename.replace(".mp3",".json")
+        dst_json_filename = src_json_filename.replace(".json","-musicbrainz.json")
 
-        if not os.path.exists(dst_json_file):
-            print(f"Querying for AcousticBrainz ID match on chromaprint fingerprint: {src_json_file}");
+        match = re.search(r"^audio/(\d{4})/([^_]+)_([^_]+)_([^_]+)\.json$",src_json_filename)
+          
+        if match:
+            year    = match.group(1)
+            country = match.group(2)
+            title   = match.group(3) # unused
+            artist  = match.group(4) # unused
+        
+        if not os.path.exists(dst_json_filename):
+            print(f"Querying for AcousticBrainz ID match on chromaprint fingerprint: {src_json_filename}");
 
-            fin = open(src_json_file)
+            fin = open(src_json_filename)
             essentia_features = json.load(fin);
             fin.close()
 
@@ -63,9 +87,9 @@ for year in opt_filtered_sorted_years:
             chromaprint_intdur = int(essentia_features["chromaprint"]["duration"])
 
             #####
-            # ****** Change client=CHANGEME to your actual client-id
+            # ****** Change the entry in acousticid_clientid.txt to your actual client-id
             #####
-            acousticid_get_args = ["client=CHANGEME","meta=releasegroup",
+            acousticid_get_args = [f"client={aid_clientid}","meta=releasegroup",
                                    f"duration={chromaprint_intdur}",f"fingerprint={chromaprint}"];
 
             acousticid_url = "https://api.acoustid.org/v2/lookup?" + "&".join(acousticid_get_args)
@@ -76,14 +100,17 @@ for year in opt_filtered_sorted_years:
 
             # can only query acoustic-id 3 time per sec
             # print(f"current time msecs = {curr_time_msecs}, prev time msecs = {prev_time_msecs}")
-            sleep_msecs = 334 - (curr_time_msecs - prev_time_msecs)
+            sleep_msecs = 334 - (curr_time_msecs - prev_time_msecs) if not prev_time_msecs is None else 0
             
             if (sleep_msecs>0):
                 sleep_secs = 0.001 * sleep_msecs
-                print("Inserting throtling delay:" + str(sleep_secs) + " seconds")
+                print("Inserting throtling delay: %.3f seconds" % sleep_secs)
                 time.sleep(sleep_secs)
             
             response = requests.get(acousticid_url);
+
+            # Made an API call, so update timing info
+            prev_time_msecs = curr_time_msecs
 
             jsonResponse = response.json();
             results = jsonResponse["results"]
@@ -95,11 +122,50 @@ for year in opt_filtered_sorted_years:
                     break
 
             if id is not None:
-                print("Found id = " + id )
-               
+                score = r["score"]
+                print(f"Found id = {id} with confidence score of {score}")
+
+
+                # metadata.json format
+                # {
+                #    "DirectoryMetadata": [
+                #        {
+                #            "FileSet": [
+                #                {
+                #                    "FileName": "Netherlands1975\\.nul"
+                #                },
+                #                {
+                #                    "Description": {
+                #                        "Metadata": [
+                #                            {
+                #                                "name": "musicbrainz.id",
+                #                                "content": "f668ff8d-797d-48b5-bb14-54675de19a35"
+                #                            }
+                #                        ]
+                #                    }
+                #                }
+                #            ]
+                #        }
+                #    ]
+                # }
+
+                filename_re = f"{country}{year}\\.nul"
+                metadata_id    = { "name" : "musicbrainz.id", "content": id }
+                metadata_score = { "score": "musicbrainz.id", "content": score }
+                
+                metadata_json = {}
+                metadata_json["DirectoryMetadata"] = [ {
+                    "FileSet": { "FileName": filename_re },
+                    "Description" : { "Metadata" : [ metadata_id, metadata_score ] }
+                } ]
+                
+                fout = open(dst_json_filename,"w")
+                json.dump(metadata_json,fout)
+                # print("**** = " + repr(metadata_json))
+                fout.close()
+                
             
         else:
-            print(f"{src_json_file} already exists => Skipping")
+            print(f"{dst_json_filename} already exists => Skipping")
 
 
-        prev_time_msecs = curr_time_msecs
